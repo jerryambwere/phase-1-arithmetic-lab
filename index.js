9122619876001683681a6250b7754eddd5b45a9a
Melvin jerry <+>
2024-07-03 17:06:46 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random() * 100);
 
-// Write your code here
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+const max = Math.max(5, 10, 15, 20);
+
+console.log("multiply:", multiply);
+console.log("random:", random);
+console.log("mod:", mod);
+console.log("max:", max);
